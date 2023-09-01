d7d1c898156126bf537199efdb26fed32dc2bf52
jarltuxen <jarl.tuxen@gmail.com>
2023-09-01 10:50:13 +0200

    getters og setters på Region.java

--- src/main/java/com/example/jpamanytoonedat22c/model/Region.java
+++ src/main/java/com/example/jpamanytoonedat22c/model/Region.java
@@ -11,4 +11,27 @@ public class Region {
     private String navn;
     private String href;
 
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
 }
