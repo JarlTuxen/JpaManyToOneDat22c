56c88d61202442e732a8fd5447c014dcbd7c07fa
Jarl Tuxen <jart@kea.dk>
2025-09-24 09:52:31 +0200

    CORS and getmapping for RegionRestController

--- src/main/java/com/example/jpamanytoonedat22c/controller/KommuneRestController.java
+++ src/main/java/com/example/jpamanytoonedat22c/controller/KommuneRestController.java
@@ -3,12 +3,14 @@ package com.example.jpamanytoonedat22c.controller;
 import com.example.jpamanytoonedat22c.model.Kommune;
 import com.example.jpamanytoonedat22c.service.ApiServiceKommunerImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 public class KommuneRestController {
 
     @Autowired
--- src/main/java/com/example/jpamanytoonedat22c/controller/RegionRestController.java
+++ src/main/java/com/example/jpamanytoonedat22c/controller/RegionRestController.java
@@ -1,22 +1,45 @@
 package com.example.jpamanytoonedat22c.controller;
 
 import com.example.jpamanytoonedat22c.model.Region;
+import com.example.jpamanytoonedat22c.repository.RegionRepository;
 import com.example.jpamanytoonedat22c.service.ApiServiceRegioner;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 public class RegionRestController {
 
     @Autowired
     ApiServiceRegioner apiServiceRegioner;
+    @Autowired
+    private RegionRepository regionRepository;
 
-    @GetMapping("/getregioner")
+    @GetMapping("/getRegioner")
     List<Region> getRegioner(){
 
         return apiServiceRegioner.getRegioner();
+    }
+
+    @GetMapping("/region")
+    List<Region> getRegion(){
+        return regionRepository.findAll();
+    }
+
+    @PostMapping("/region")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Region postRegion(@RequestBody Region region){
+        System.out.println(region);
+        return regionRepository.save(region);
+    }
+
+    @PostMapping("/region2")
+    public ResponseEntity<String> postRegion2(@RequestParam String kode,
+                                              @RequestParam String navn){
+        return ResponseEntity.ok("Received: " + kode + " " + navn);
     }
 }
